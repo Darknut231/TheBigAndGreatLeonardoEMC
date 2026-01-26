d30c5d9123024f3ae1311258446b0916a6fb77be
Данил <Darknuttop@gmail.com>
2026-01-26 10:31:20 +0000

    ts runs

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import plot_tree
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+print('helo')
