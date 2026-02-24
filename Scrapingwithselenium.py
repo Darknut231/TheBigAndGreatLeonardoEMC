f98ccf0b2a95901faf5c534012ded3283361d1b8
AlexWW123 <alexwwinterbottom@gmail.com>
2026-02-24 12:56:14 +0000

    Update Scrapingwithselenium.py

--- Scrapingwithselenium.py
+++ Scrapingwithselenium.py
@@ -2,35 +2,36 @@
 
 import selenium
 from selenium import webdriver#importing things
+from selenium.webdriver.common.by import By
 from pandas import pd #helpful for exporting data
 
-url= 'https://www.youtube.com/@veritasium/videos'# setting our url
+url= 'https://www.helis.com/database/model/AW109E/cn'# setting our url
 driver=webdriver.Chrome()#defining what webdriver we want to use
 driver.get(url)#opens the url we told it earlier
 
+#mitabla# the class of the section of the page which contains all the nessecary info
+#//*[@id="trcnmod11046"]/td[4]/b#copied 'xpath' of the location of the title of the vid when inspecting the page
+#//*[@id="metadata-line"]/span[1]#copied xpath of the views of the video
+#//*[@id="metadata-line"]/span[2]#copied xpath of the age of the video
 
-style-scope ytd-rich-item-renderer# the class of the section of the page which contains all the nessecary info
-//*[@id="video-title"]#copied 'xpath' of the location of the title of the vid when inspecting the page
-//*[@id="metadata-line"]/span[1]#copied xpath of the views of the video
-//*[@id="metadata-line"]/span[2]#copied xpath of the age of the video
+rows=driver.find_elements_by_class_name('mitabla')#finds all the things (elements) within the class: style.scope ect
 
-videos=driver.find_elements_by_class_name('style-scope ytd-rich-item-renderer')#finds all the things (elements) within the class: style.scope ect
+for row in rows: #this loops through all the rows in the page and collects the info from them
+   # title= row.find_element_by_xpath('.//*[@id="trcnmod11046"]/td[4]/b').text#within all elements that match our class name, store it in rows, then for each one in rows, store it in row. Then look in the variable row for the element which matches the id thing. Dont forget the dot. The .text allows us to only get the text out of it rather than anything else
+    title = row.find_elements(By.XPATH, "//tr[starts-with(@id, 'trcnmod')]").text# this seems to be the new method so idk if either will work
+    country= row.find_element_by_xpath('.//*[@id="trcnmod11046"]/td[4]/img').text #same again for veiws
+    year= row.find_element_by_xpath('.//*[@id="trcnmod11046"]/td[3]').text#and again for age
 
-for video in videos #this loops through all the videos in the page and collects the info from them
-    title= video.find_element_by_xpath('.//*[@id="video-title"]').text#within all elements that match our class name, store it in videos, then for each one in videos, store it in video. Then look in the variable video for the element which matches the id thing. Dont forget the dot. The .text allows us to only get the text out of it rather than anything else
-    veiws= video.find_element_by_xpath('.//*[@id="metadata-line"]/span[1]').text #same again for veiws
-    age= video.find_element_by_xpath('.//*[@id="metadata-line"]/span[2]').text#and again for age
-
-#print(title,veiws,age)#just checking it works
+    print(title)#just checking it works
 
     #Now for using panda to export stuff
-    video_list[]
+    row_list=[]
     vid_item ={#storing all the info into a 'dictionary'
         'title': title,
-        'veiws': veiws,
-        'age': age
+        'Country': country,
+       'Year': year
         }
-    video_list.append(vid_item)#adding the dictionary stuff to a list
+    row_list.append(vid_item)#adding the dictionary stuff to a list
 
-df= pd.DataFrame(video_list)
+df= pd.DataFrame(row_list)
 print(df)
