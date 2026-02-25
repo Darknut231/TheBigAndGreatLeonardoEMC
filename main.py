a3e6107972688125806656192bf747b95db2e91f
Данил <Darknuttop@gmail.com>
2026-02-25 19:31:57 +0000

    stufff

--- main.py
+++ main.py
@@ -6,23 +6,42 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import plot_tree
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.svm import SVC
+# reading the file
 df = pd.read_csv('the big and great dataset (TBaGD).csv')
-#print(df.dtypes)
+
+# removing missing data
 df_no_missing = df.loc[(df["cost(mil$)"] != "Unknown") & (df["cost(mil$)"] != "") & (df["Date of order"] != "Unkown") & (df["Still operatable?"] != "?")]
 X = df_no_missing.drop('Successfull procurement',axis=1).copy()
 X = X.drop('Model of the helicopter',axis=1).copy()
-X = X.drop('GDP billions $ of the owner',axis=1).copy()
+X = X.drop('Still operatable?',axis=1).copy()
 y = df_no_missing['Successfull procurement'].copy()
-X_train, X_test, y_train, y_test = train_test_split(X,y,random_state=20)
-clf_dt = DecisionTreeClassifier(random_state=10)
+
+# one hot encoding
+X_encoded = pd.get_dummies(X,columns=['cost(mil$)','Date of order','GDP billions $ of the owner'])
+print(X_encoded)
+# splitting the data
+X_train, X_test, y_train, y_test = train_test_split(X_encoded,y,random_state=25)
+clf_dt = DecisionTreeClassifier(random_state=15)
 clf_dt = clf_dt.fit(X_train,y_train)
-plt.figure(figsize=(15,7.5))
-plot_tree(clf_dt,
-          filled=True,
-          rounded=True,
-          class_names=["Success","Unsuccess"],
-          feature_names=X.columns)
+def plotTree(clf_dt,X):
+    plt.figure(figsize=(15,7.5))
+    plot_tree(clf_dt,
+            filled=True,
+            rounded=True,
+            class_names=["Success","Unsuccess"],
+            feature_names=X.columns)
+plot_tree(clf_dt,X)
+
+#too many positional arguments???
+
+clf = SVC(random_state=0)
+clf.fit(X_train, y_train)
+predictions = clf.predict(X_test)
+cm = confusion_matrix(y_test, predictions, labels=clf.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=clf.classes_)
+disp.plot() # here the matrix is displayed
+
 plt.show()
 #df_no_missing = df with no missings in cost or date =)
