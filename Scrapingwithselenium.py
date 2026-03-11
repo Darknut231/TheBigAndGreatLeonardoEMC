01a2ab7ef1086a9a74dcaa0173d7b76ef7a750b7
AlexWW123 <alexwwinterbottom@gmail.com>
2026-03-11 16:51:35 +0000

    Update Scrapingwithselenium.py

--- Scrapingwithselenium.py
+++ Scrapingwithselenium.py
@@ -12,12 +12,16 @@ from selenium.webdriver.support import expected_conditions as EC
 driver=webdriver.Chrome()
 row_list=[]
 
-#Diferent Helicopter:
+driver.get('https://www.helis.com/database')
+WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, '//*[@class="fc-button fc-cta-consent fc-primary-button"]'))).click()
+
+#First Helicopter:
 for page in range(1, 9):  # this loops through all the pages and collects the data from them
     url = f"https://www.helis.com/database/model/AW109/cn?pag={page}"
     driver.get(url)
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@class="fc-button fc-cta-consent fc-primary-button"]'))).click()
-
+    # if page==1:
+    #    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@class="fc-button fc-cta-consent fc-primary-button"]'))).click()
+    
     rows = driver.find_elements(By.XPATH, "//tr[starts-with(@id,'trcnmod')]")
 
     for row in rows: 
@@ -62,55 +66,55 @@ for page in range(1, 9):  # this loops through all the pages and collects the da
             }
         row_list.append(vid_item)
 
-#New Helicopter
-for page in range(1, 9):  # this loops through all the pages and collects the data from them
-    url = f"https://www.helis.com/database/model/AW109/cn?pag={page}"
-    driver.get(url)
-    time.sleep(3) #Press Consent
+#Diferent Helicopter
+# for page in range(1, 9):  # this loops through all the pages and collects the data from them
+#     url = f"https://www.helis.com/database/model/AW109/cn?pag={page}"
+#     driver.get(url)
+#     time.sleep(3) #Press Consent
 
-    rows = driver.find_elements(By.XPATH, "//tr[starts-with(@id,'trcnmod')]")
+#     rows = driver.find_elements(By.XPATH, "//tr[starts-with(@id,'trcnmod')]")
 
-    for row in rows: 
+#     for row in rows: 
         
-        #MODEL
-        model=driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/h1').text
+#         #MODEL
+#         model=driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/h1').text
         
-        #TAIL NUMBER
-        #code = row.find_element(By.XPATH, ".//td[5]/b").text  
+#         #TAIL NUMBER
+#         #code = row.find_element(By.XPATH, ".//td[5]/b").text  
         
-        #YEAR
-        if len(row.find_element(By.XPATH, ".//td[4]").text) > 0:
-            year = row.find_element(By.XPATH, ".//td[4]").text
-        else:
-            year='NA'                                           
+#         #YEAR
+#         if len(row.find_element(By.XPATH, ".//td[4]").text) > 0:
+#             year = row.find_element(By.XPATH, ".//td[4]").text
+#         else:
+#             year='NA'                                           
         
 
-        #COUNTRY, This bit is ai so idk how it works sry
-        imgs = row.find_elements(By.XPATH, ".//td[5]/img")
+#         #COUNTRY, This bit is ai so idk how it works sry
+#         imgs = row.find_elements(By.XPATH, ".//td[5]/img")
 
-        countries = [img.get_attribute("alt") for img in imgs if img.get_attribute("alt")]
+#         countries = [img.get_attribute("alt") for img in imgs if img.get_attribute("alt")]
         
-        if not countries:
-            countries= 'NA'
-            #continue
-        country_string = ", ".join(countries)
+#         if not countries:
+#             countries= 'NA'
+#             #continue
+#         country_string = ", ".join(countries)
 
-        #STILL OPERABLE
-        try:
-            variable=row.find_element(By.XPATH,".//td[2]/i")
-            operable='False'
-        except NoSuchElementException:
-            operable='True'
+#         #STILL OPERABLE
+#         try:
+#             variable=row.find_element(By.XPATH,".//td[2]/i")
+#             operable='False'
+#         except NoSuchElementException:
+#             operable='True'
 
-        #Exporting The Data
-        vid_item ={
-            #'Tail Number': code,
-            'Country': countries,
-            'Year': year,
-            'Still Operational':operable,
-            'Model': model
-            }
-        row_list.append(vid_item)
+#         #Exporting The Data
+#         vid_item ={
+#             #'Tail Number': code,
+#             'Country': countries,
+#             'Year': year,
+#             'Still Operational':operable,
+#             'Model': model
+#             }
+#         row_list.append(vid_item)
 
 df= pd.DataFrame(row_list)
 print(df)
