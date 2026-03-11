fd7fbb70e4e9a9244e24ce42e62282a977172266
AlexWW123 <alexwwinterbottom@gmail.com>
2026-03-11 17:03:15 +0000

    Update Scrapingwithselenium.py

--- Scrapingwithselenium.py
+++ Scrapingwithselenium.py
@@ -15,13 +15,12 @@ row_list=[]
 driver.get('https://www.helis.com/database')
 WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, '//*[@class="fc-button fc-cta-consent fc-primary-button"]'))).click()
 
-#First Helicopter:
+#First Helicopter,  Ofc all the parameters need changing
 for page in range(1, 9):  # this loops through all the pages and collects the data from them
     url = f"https://www.helis.com/database/model/AW109/cn?pag={page}"
     driver.get(url)
-    # if page==1:
-    #    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@class="fc-button fc-cta-consent fc-primary-button"]'))).click()
-    
+    time.sleep(3) #Press Consent
+
     rows = driver.find_elements(By.XPATH, "//tr[starts-with(@id,'trcnmod')]")
 
     for row in rows: 
@@ -56,9 +55,9 @@ for page in range(1, 9):  # this loops through all the pages and collects the da
         except NoSuchElementException:
             operable='True'
 
-        #Exporting The Data
+        #ADDING THE DATA TO THE DATA FRAME
         vid_item ={
-         #   'Tail Number': code,
+            #'Tail Number': code,
             'Country': countries,
             'Year': year,
             'Still Operational':operable,
@@ -66,56 +65,58 @@ for page in range(1, 9):  # this loops through all the pages and collects the da
             }
         row_list.append(vid_item)
 
-#Diferent Helicopter
-# for page in range(1, 9):  # this loops through all the pages and collects the data from them
-#     url = f"https://www.helis.com/database/model/AW109/cn?pag={page}"
-#     driver.get(url)
-#     time.sleep(3) #Press Consent
+#Diferent Helicopter#  Ofc all the parameters need changing
+for page in range(1, 9):  # this loops through all the pages and collects the data from them
+    url = f"https://www.helis.com/database/model/AW109/cn?pag={page}"
+    driver.get(url)
+    time.sleep(3) #Press Consent
 
-#     rows = driver.find_elements(By.XPATH, "//tr[starts-with(@id,'trcnmod')]")
+    rows = driver.find_elements(By.XPATH, "//tr[starts-with(@id,'trcnmod')]")
 
-#     for row in rows: 
+    for row in rows: 
         
-#         #MODEL
-#         model=driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/h1').text
+        #MODEL
+        model=driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/h1').text
         
-#         #TAIL NUMBER
-#         #code = row.find_element(By.XPATH, ".//td[5]/b").text  
+        #TAIL NUMBER
+        #code = row.find_element(By.XPATH, ".//td[5]/b").text  
         
-#         #YEAR
-#         if len(row.find_element(By.XPATH, ".//td[4]").text) > 0:
-#             year = row.find_element(By.XPATH, ".//td[4]").text
-#         else:
-#             year='NA'                                           
+        #YEAR
+        if len(row.find_element(By.XPATH, ".//td[4]").text) > 0:
+            year = row.find_element(By.XPATH, ".//td[4]").text
+        else:
+            year='NA'                                           
         
 
-#         #COUNTRY, This bit is ai so idk how it works sry
-#         imgs = row.find_elements(By.XPATH, ".//td[5]/img")
+        #COUNTRY, This bit is ai so idk how it works sry
+        imgs = row.find_elements(By.XPATH, ".//td[5]/img")
 
-#         countries = [img.get_attribute("alt") for img in imgs if img.get_attribute("alt")]
+        countries = [img.get_attribute("alt") for img in imgs if img.get_attribute("alt")]
         
-#         if not countries:
-#             countries= 'NA'
-#             #continue
-#         country_string = ", ".join(countries)
+        if not countries:
+            countries= 'NA'
+            #continue
+        country_string = ", ".join(countries)
 
-#         #STILL OPERABLE
-#         try:
-#             variable=row.find_element(By.XPATH,".//td[2]/i")
-#             operable='False'
-#         except NoSuchElementException:
-#             operable='True'
+        #STILL OPERABLE
+        try:
+            variable=row.find_element(By.XPATH,".//td[2]/i")
+            operable='False'
+        except NoSuchElementException:
+            operable='True'
 
-#         #Exporting The Data
-#         vid_item ={
-#             #'Tail Number': code,
-#             'Country': countries,
-#             'Year': year,
-#             'Still Operational':operable,
-#             'Model': model
-#             }
-#         row_list.append(vid_item)
+        #ADDING THE DATA TO THE DATA FRAME
+        vid_item ={
+            #'Tail Number': code,
+            'Country': countries,
+            'Year': year,
+            'Still Operational':operable,
+            'Model': model
+            }
+        row_list.append(vid_item)
 
+#EXPORTING OUR DATA FRAME
 df= pd.DataFrame(row_list)
 print(df)
-df.to_excel(r"C:\Users\alexw\OneDrive\Documents\EMS\EMC\Data Dump- Tests\EMCDATA.xlsx", index=False)# this will now go in the folder, to change the name change teh EMCDATA0 to smh else
+#CHANGE THE LOCATION TO THE LOACATION OF YOUR FOLDER WHERE YOU WANT THE FILE TO BE PLACED
+df.to_excel(r"C:\Users\alexw\OneDrive\Documents\EMS\EMC\Data Dump- Tests\EMCDATA.xlsx", index=False)# 
